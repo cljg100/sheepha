829ef1b1d397fb0682a23c0d6355f4456534ccab
Claudia J <cljg01@gmail.com>
2021-10-07 17:03:28 -0300

    lbalancing

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ resource "oci_core_internet_gateway" "tcb_internetgateway1" {
 
 resource "oci_core_route_table" "tcb_routetable1" {
   compartment_id = var.compartment_ocid
-  vcn_id         = oci_core_vcn.vcn1.id
+  vcn_id         = oci_core_vcn.tcb_vcn1.id
   display_name   = "tcb_routetable1"
 
   route_rules {
