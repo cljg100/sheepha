65805d127193ade2f486b6772efba366d42db003
Claudia J <cljg01@gmail.com>
2021-10-07 00:04:37 -0300

    atualizando...

--- main.tf
+++ main.tf
@@ -1,49 +1,136 @@
 // Copyright (c) 2017, 2021, Oracle and/or its affiliates. All rights reserved.
 // Licensed under the Mozilla Public License v2.0
 
-/*
- * This example demonstrates round robin load balancing behavior by creating two instances, a configured
- * vcn and a load balancer. The public IP of the load balancer is outputted after a successful run, curl
- * this address to see the hostname change as different instances handle the request.
- *
- * NOTE: The https listener is included for completeness but should not be expected to work,
- * it uses dummy certs.
- */
+
+variable "compartment_ocid" {}
+variable "region" {}
 variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
-variable "compartment_ocid" {}
 variable "private_key" {}
-variable "region" {}
-variable "instance_image_ocid" {
+variable "ssh_public_key" {}
+
+provider "oci" {
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid = var.user_ocid
+  fingerprint = var.fingerprint
+  private_key = var.private_key
+  region = var.region
+}
+
+variable "ad_region_mapping" {
   type = map(string)
+
   default = {
-    # See https://docs.us-phoenix-1.oraclecloud.com/images/
-    # Oracle-provided image "Oracle-Linux-7.5-2018.10.16-0"
-    us-phoenix-1   = "ocid1.image.oc1.phx.aaaaaaaaoqj42sokaoh42l76wsyhn3k2beuntrh5maj3gmgmzeyr55zzrwwa"
-    us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaageeenzyuxgia726xur4ztaoxbxyjlxogdhreu3ngfj2gji3bayda"
-    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaitzn6tdyjer7jl34h2ujz74jwy5nkbukbh55ekp6oyzwrtfa4zma"
-    uk-london-1    = "ocid1.image.oc1.uk-london-1.aaaaaaaa32voyikkkzfxyo4xbdmadc2dmvorfxxgdhpnk6dw64fa3l4jh7wa"
+    us-phoenix-1 = 3
+    us-ashburn-1 = 2
+    sa-saopaulo-1 = 1
   }
 }
-variable "instance_shape" {
-  default = "VM.Standard2.1"
+
+variable "images" {
+  type = map(string)
+
+  default = {
+    # See https://docs.us-phoenix-1.oraclecloud.com/images/
+    # Oracle-provided image "Oracle-Linux-7.9-2020.10.26-0"
+    us-phoenix-1   = "ocid1.image.oc1.phx.aaaaaaaacirjuulpw2vbdiogz3jtcw3cdd3u5iuangemxq5f5ajfox3aplxa"
+    us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaabbg2rypwy5pwnzinrutzjbrs3r35vqzwhfjui7yibmydzl7qgn6a"
+    sa-saopaulo-1   = "ocid1.image.oc1.sa-saopaulo-1.aaaaaaaaudio63gdicxwujhfok7jdyewf6iwl6sgcaqlyk4fvttg3bw6gbpq"
+  }
 }
-variable "availability_domain" {
-  default = 3
-}
-provider "oci" {
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key      = var.private_key
-  region           = var.region
-}
-data "oci_identity_availability_domain" "ad1" {
+
+data "oci_identity_availability_domain" "ad" {
   compartment_id = var.tenancy_ocid
-  ad_number      = 1
+  ad_number      = var.ad_region_mapping[var.region]
 }
-data "oci_identity_availability_domain" "ad2" {
-  compartment_id = var.tenancy_ocid
-  ad_number      = 2
+
+resource "oci_core_virtual_network" "tcb_vcn" {
+  cidr_block     = "10.1.0.0/16"
+  compartment_id = var.compartment_ocid
+  display_name   = "tcbVCN"
+  dns_label      = "tcbvcn"
+}
+
+resource "oci_core_subnet" "tcb_subnet" {
+  cidr_block        = "10.1.20.0/24"
+  display_name      = "tcbSubnet"
+  dns_label         = "tcbsubnet"
+  security_list_ids = [oci_core_security_list.tcb_security_list.id]
+  compartment_id    = var.compartment_ocid
+  vcn_id            = oci_core_virtual_network.tcb_vcn.id
+  route_table_id    = oci_core_route_table.tcb_route_table.id
+  dhcp_options_id   = oci_core_virtual_network.tcb_vcn.default_dhcp_options_id
+}
+
+resource "oci_core_internet_gateway" "tcb_internet_gateway" {
+  compartment_id = var.compartment_ocid
+  display_name   = "tcbIG"
+  vcn_id         = oci_core_virtual_network.tcb_vcn.id
+}
+
+resource "oci_core_route_table" "tcb_route_table" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.tcb_vcn.id
+  display_name   = "tcbRouteTable"
+
+  route_rules {
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_internet_gateway.tcb_internet_gateway.id
+  }
+}
+
+resource "oci_core_security_list" "tcb_security_list" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.tcb_vcn.id
+  display_name   = "tcbSecurityList"
+
+  egress_security_rules {
+    protocol    = "6"
+    destination = "0.0.0.0/0"
+  }
+
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
+
+    tcp_options {
+      max = "22"
+      min = "22"
+    }
+  }
+
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
+
+    tcp_options {
+      max = "80"
+      min = "80"
+    }
+  }
+}
+
+resource "oci_core_instance" "webserverha1" {
+  availability_domain = data.oci_identity_availability_domain.ad.name
+  compartment_id      = var.compartment_ocid
+  display_name        = "webserver1"
+  shape               = "VM.Standard.E2.1.Micro"
+
+  create_vnic_details {
+    subnet_id        = oci_core_subnet.tcb_subnet.id
+    display_name     = "primaryvnic"
+    assign_public_ip = true
+    hostname_label   = "webserverha1"
+  }
+
+  source_details {
+    source_type = "image"
+    source_id   = var.images[var.region]
+  }
+
+  metadata = {
+    ssh_authorized_keys = var.ssh_public_key
+  }
 }
