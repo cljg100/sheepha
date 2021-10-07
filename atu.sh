c9d839988283dd1012b03688d0deed3b01bbfe61
Claudia J <cljg01@gmail.com>
2021-10-07 07:50:37 -0300

    atu 8

---
+++ atu.sh
@@ -0,0 +1,4 @@
+git add main.tf
+git commit -m "atualizando main.tf"
+git branch -M main
+git push -u origin main
---
+++ install-website.sh
@@ -0,0 +1,4 @@
+#ssh opc@<ip-público-da-VM> -i <chave-privada-gerada>
+wget https://objectstorage.us-ashburn-1.oraclecloud.com/p/_taLFTuy_AYrS2PloNwMKVGI-pXqJLjeOC_iXNrutee9xXYuOYMBcqlK8SQO_QuH/n/idqfa2z2mift/b/bootcamp-oci/o/deploy_niture.sh
+chmod +x deploy_niture.sh
+# ./deploy_niture.sh
