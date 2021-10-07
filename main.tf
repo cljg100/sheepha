05d6107a286fb465bade44b46530ad9dd01057f8
Claudia J <cljg01@gmail.com>
2021-10-07 06:29:21 -0300

    atu -3

--- main.tf
+++ main.tf
@@ -111,3 +111,42 @@ resource "oci_core_security_list" "tcb_security_list" {
     }
   }
 }
+
+resource "oci_core_instance" "webserverha1" {
+  availability_domain = data.oci_identity_availability_domain.ad.name
+  compartment_id      = var.compartment_ocid
+  display_name        = "webserverha1"
+  shape               = "VM.Standard.E2.1.Micro"
+
+  create_vnic_details {
+    subnet_id        = oci_core_subnet.tcb_subnet.id
+    display_name     = "primaryvnic"
+    assign_public_ip = true
+    hostname_label   = "webserverha1"
+  }
+
+  source_details {
+    source_type = "image"
+    source_id   = var.images[var.region]
+  }
+
+  metadata = {
+    user_data = base64encode(var.user-data)
+  }
+}
+variable "user-data" {
+default = <<EOF
+#!/bin/bash -x
+yum install httpd -y
+apachectl start
+systemctl enable httpd
+firewall-cmd --zone=public --add-service=http
+firewall-cmd --permanent --zone=public --add-service=http
+echo "chamei aqui" > /var/www/html/teste.txt
+cd /var/www/html/
+wget https://objectstorage.us-ashburn-1.oraclecloud.com/p/u8j40_AS-7pRypC5boQT24w5QFPDTy-0j27BWBOfmsxbERTiuDtJQBIqfcsOH81F/n/idqfa2z2mift/b/bootcamp-oci/o/oci-f-handson-modulo-compute-website-files.zip
+unzip oci-f-handson-modulo-compute-website-files.zip
+chown -R apache:apache /var/www/html
+rm -rf oci-f-handson-modulo-compute-website-files.zip
+EOF
+}
